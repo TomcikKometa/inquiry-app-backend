f7bbe18d71118dccc3903425c9dbf4136d4f5775
TomcikKometa <tomcik48_84@o2.pl>
2024-04-30 10:52:34 +0200

    add entity to module,remove feautres from useGlobalPipes

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -7,12 +7,14 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { LogInterceptor } from './app/interceptors/log.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{cors:true});
   const globalPrefix = 'api';
+  app.useGlobalInterceptors(new LogInterceptor());
   app.setGlobalPrefix(globalPrefix);
-  app.useGlobalPipes(new ValidationPipe({transform:true,whitelist:true}))
+  app.useGlobalPipes(new ValidationPipe())
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
