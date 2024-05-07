7dc3b27112f220af286d25f96532b524e83ff4e4
TomcikKometa <tomcik48_84@o2.pl>
2024-05-07 10:31:00 +0200

    change function saveInquiry

--- apps/backend/src/app/services/inquiry/inquiry.service.ts
+++ apps/backend/src/app/services/inquiry/inquiry.service.ts
@@ -8,6 +8,8 @@ import { InquiryDto } from './model/inquiry-dto';
 import { Question } from '../../entities/question';
 import { QuestionDto } from './model/question-dto';
 import { QuestionType } from '../../entities/enums/question-type';
+import { ShortTextQuestion } from '../../entities/short-text-question';
+import { ShortTextQuestionDto } from './model/shortTextQuestionDto';
 
 @Injectable()
 export class InquiryService {
@@ -15,14 +17,21 @@ export class InquiryService {
     @InjectRepository(Inquiry)
     private inquiryRepository: Repository<Inquiry>,
     @InjectRepository(Question)
-    private questionRepository: Repository<Question>) {}
+    private questionRepository: Repository<Question>,
+    @InjectRepository(ShortTextQuestion)
+    private shortTextQuestionRepository: Repository<ShortTextQuestion>
+  ) {}
 
   public async getAll(): Promise<InquiryDto[]> {
-    return (await this.inquiryRepository.find()).map((entity:Inquiry) => (this.entityToDto(entity)));
+    return (await this.inquiryRepository.find()).map((entity: Inquiry) =>
+      this.entityToDto(entity)
+    );
   }
 
   public async getById(id: number): Promise<InquiryDto | null> {
-    const entityInquiry:Inquiry = await this.inquiryRepository.findOneBy({ ID: id });
+    const entityInquiry: Inquiry = await this.inquiryRepository.findOneBy({
+      ID: id,
+    });
     return this.entityToDto(entityInquiry);
   }
 
@@ -30,33 +39,45 @@ export class InquiryService {
     const inquiry = new Inquiry();
     inquiry.Name = request.name;
     await this.inquiryRepository.save(inquiry);
-
-    await this.questionRepository.save(request.questions.map((question:QuestionDto)=> new Question(question.type,question.label,inquiry)));
-    
+    const questions = request.questions.map(async (questionDto: QuestionDto) => {
+        const question: Question = new Question(questionDto.type,questionDto.label,inquiry);
+        if (questionDto.type === QuestionType.SHORT_TEXT) {
+          const shortTextQuestion: ShortTextQuestion = await this.shortTextQuestionRepository.save(new ShortTextQuestion((questionDto as ShortTextQuestionDto).answer));
+          question.shortTextQuestion = shortTextQuestion;
+        }
+        return question;
+      }
+    );
+    await this.questionRepository.save(await Promise.all(questions));
   }
 
   public async editInquiry(id: number, request: EditInquryRequest) {
-    const inquiryToEdit: Inquiry | null = await this.inquiryRepository.findOneBy({ID:id})
+    const inquiryToEdit: Inquiry | null =
+      await this.inquiryRepository.findOneBy({ ID: id });
     if (inquiryToEdit) {
       inquiryToEdit.Name = request.name;
       this.inquiryRepository.save(inquiryToEdit);
     }
   }
 
-  public async deleteInquiryRequest(id:number): Promise<void>{
-    await this.inquiryRepository.findOneBy({ID:id}).then(async (value:Inquiry) => { await this.inquiryRepository.delete(value)})
+  public async deleteInquiryRequest(id: number): Promise<void> {
+    await this.inquiryRepository
+      .findOneBy({ ID: id })
+      .then(async (value: Inquiry) => {
+        await this.inquiryRepository.delete(value);
+      });
   }
 
-  private entityToDto(entity:Inquiry): InquiryDto {
+  private entityToDto(entity: Inquiry): InquiryDto {
     return {
-      id : entity.ID,
-      name : entity.Name,
-      questions:entity.questions?.map((question:Question) => {
+      id: entity.ID,
+      name: entity.Name,
+      questions: entity.questions?.map((question: Question) => {
         return {
-          label : question.label,
-          type : question.questionType as QuestionType
-        }
-      })
+          label: question.label,
+          type: question.questionType as QuestionType,
+        };
+      }),
     };
   }
 }
