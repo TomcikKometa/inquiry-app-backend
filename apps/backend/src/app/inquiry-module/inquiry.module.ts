aa35d574216eeb2ec45186307ba9ff313fda9d88
TomcikKometa <tomcik48_84@o2.pl>
2024-05-17 10:59:57 +0200

    add entites to inquiry module

--- apps/backend/src/app/inquiry-module/inquiry.module.ts
+++ apps/backend/src/app/inquiry-module/inquiry.module.ts
@@ -5,9 +5,13 @@ import { InquiryService } from '../services/inquiry/inquiry.service';
 import { InquiryController } from '../controllers/inquiry.controller';
 import { Question } from '../entities/question';
 import { ShortTextQuestion } from '../entities/short-text-question';
+import { MultiSelectQuestion } from '../entities/multi-select-question';
+import { SingleSelectQuestion } from '../entities/single-select-question';
+import { MultiSelectAnswer } from '../entities/multi-select-answer';
+import { SingleSelectAnswer } from '../entities/single-select-answer';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Inquiry,Question,ShortTextQuestion])],
+    imports: [TypeOrmModule.forFeature([Inquiry,Question,ShortTextQuestion,MultiSelectQuestion,SingleSelectQuestion,MultiSelectAnswer,SingleSelectAnswer])],
     providers: [InquiryService],
     controllers: [InquiryController],
   })
