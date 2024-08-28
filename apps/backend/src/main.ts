96b9798e9b9b7b6a7a62141a7d7ed39f542d8be1
TomcikKometa <tomcik48_84@o2.pl>
2024-08-28 12:37:27 +0200

    add to config app authBearer properties"

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -11,26 +11,35 @@ import { LogInterceptor } from './app/interceptors/log.interceptor';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{cors:true});
+  const app = await NestFactory.create(AppModule, { cors: true });
   const globalPrefix = 'api';
   app.useGlobalInterceptors(new LogInterceptor());
   app.setGlobalPrefix(globalPrefix);
-  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalPipes(new ValidationPipe());
   const port = process.env.PORT || 3000;
 
   const config = new DocumentBuilder()
     .setTitle('Inquiry Api')
     .setDescription('Api endpoints')
     .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'token',
+        in: 'header',
+      },
+      'JWT-auth'
+    )
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
   await app.listen(port);
 
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
 
 bootstrap();
